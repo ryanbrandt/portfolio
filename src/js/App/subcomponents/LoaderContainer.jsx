1e96e590615413add1b82fb22d4334a37a6f88eb
Ryan Brandt <rnb52@scarletmail.rutgers.edu>
2019-12-18 00:17:32 -0500

    add message to loader when server is asleep

--- src/js/App/subcomponents/LoaderContainer.jsx
+++ src/js/App/subcomponents/LoaderContainer.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Loader, Dimmer, Transition } from "semantic-ui-react";
+
+function useForceUpdate() {
+  const [value, setValue] = useState(true);
+  return () => setValue(!value);
+}
+
+let loadingMessage = "Loading ...";
 
 const LoaderContainer = props => {
   const { show } = props;
+
+  const update = useForceUpdate();
+
+  const timer = setTimeout(() => {
+    loadingMessage = "Server waking up ...";
+    update();
+  }, 4000);
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timer);
+    };
+  });
 
   return (
     <Transition.Group animation="fade" duration={500}>
       {show && (
         <Dimmer active page>
-          <Loader size="huge" content="Loading ..." indeterminate />
+          <Loader size="huge" content={loadingMessage} indeterminate />
         </Dimmer>
       )}
     </Transition.Group>
